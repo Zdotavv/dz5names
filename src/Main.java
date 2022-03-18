ab0acd507e97cb4e72dabdb8857a19b40bd92d94
Vladimir <zdotavv@ukr.net>
2022-03-18 14:56:13 +0200

    Add Mike

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Names: ");
         System.out.println("-Alex;");
-
+        System.out.println("-Mike;");
     }
 }
